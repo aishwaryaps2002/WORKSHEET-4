52a97b24c03e91e59ba64c98f1c8eb548e7e5dba
aishwaryaps2002 <133131171+aishwaryaps2002@users.noreply.github.com>
2025-09-22 21:47:51 +0530

    Constructors.cpp

--- PROGRAM 1/Constructors.cpp
+++ PROGRAM 1/Constructors.cpp
@@ -1,56 +1,57 @@
+
 #include <iostream>
 using namespace std;
-class Complex 
+class Complex
 {
 private:
     float real;
     float imag;
 public:
-    Complex() 
-{
+    Complex()
+    {
         real = 0;
         imag = 0;
         cout << "Default constructor called" << endl;
-}
-    Complex(float r, float i) 
-{
+    }
+    Complex(float r, float i)
+    {
         real = r;
         imag = i;
         cout << "Parameterized constructor called" << endl;
-}
+    }
 
-    Complex(const Complex &c) 
-{
+    Complex(const Complex &c)
+    {
         real = c.real;
         imag = c.imag;
         cout << "Copy constructor called" << endl;
-}
+    }
 
-    ~Complex() 
-{
+    ~Complex()
+    {
         cout << "Destructor called for Complex(" << real << ", " << imag << ")" << endl;
-}
+    }
 
-    void getdata() 
-{
+    void getdata()
+    {
         cout << "Enter real part: ";
         cin >> real;
         cout << "Enter imaginary part: ";
         cin >> imag;
-}
-    void showdata() 
-{
+    }
+    void showdata()
+    {
         cout << real << " + " << imag << "i" << endl;
-}
-    Complex add_complex(Complex c1, Complex c2) 
-{
+    }
+    Complex add_complex(Complex c1, Complex c2)
+    {
         Complex temp;
         temp.real = c1.real + c2.real;
         temp.imag = c1.imag + c2.imag;
         return temp;
-}
+    }
 };
-int main() 
+int main()
 {
     Complex c1, c2, c3;
     cout << "\nEnter first complex number:" << endl;
@@ -69,3 +70,4 @@ int main()
     c4.showdata();
     return 0;
 }
+
